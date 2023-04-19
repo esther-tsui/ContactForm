f65cb7baa571e4de80e8e08e9230f4ca229107b6
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2023-04-19 18:27:29 +0100

    first commit

---
+++ Contact_Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact_Form
+//
+//  Created by Wing Hei Tsui on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
